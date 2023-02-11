b8daa2ea080465c260f6d14801c750ed60361197
«Ksenia» <ksenia_slys@mail.ru>
2023-02-11 16:58:23 +0300

    Ex1.2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,8 +10,8 @@ if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
-Console.Write("Максимальное число = ");
-Console.WriteLine(max);
+Console.Write("Максимальное число = " + max);
+
 
 
 
