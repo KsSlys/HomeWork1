8d907612bbf55d8123846309c99f4ebd9ce6fd61
«Ksenia» <ksenia_slys@mail.ru>
2023-02-11 16:57:38 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number < N)
+{
+    if (number % 2 == 1)
+    {
+        number ++;
+    }
+    else
+    {
+        Console.WriteLine(number + ", ");
+        number ++;
+    }
+}
