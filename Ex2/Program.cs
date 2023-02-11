102bce48c85d2bb88470e4b25996c1a384118d90
«Ksenia» <ksenia_slys@mail.ru>
2023-02-11 16:41:26 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 1)
+{
+    Console.WriteLine("Нечетное");
+}
+else
+{
+    Console.WriteLine("Четное");
+}
